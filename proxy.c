ce36675b1dfd5555a18073e5d2250307f3f43fc8
Tomas853 <tomas.gebrewold@wsu.edu>
2025-11-30 15:53:52 -0800

    web proxy complete

--- proxy.c
+++ proxy.c
@@ -1,8 +1,5 @@
 #include "proxy.h"
 
-/* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
-
 int main(int argc, char **argv)
 {
     /* Check command line args */
@@ -11,32 +8,59 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    int proxySocket, clientSocket, serverSocket;
+    int proxySocket, clientSocket;
     struct sockaddr_in proxy_address;
     int proxyPort = atoi(argv[1]);
 
 
     proxySocket = Socket(AF_INET, SOCK_STREAM, 0);
+
+    int opt = 1;
+    Setsockopt(proxySocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
     
     memset(proxy_address.sin_zero, 0, sizeof(proxy_address.sin_zero));  //set all bytes to zero before use. prevents unexpected behavior from uninitialiazed memory
     proxy_address.sin_family = AF_INET;
     proxy_address.sin_port = htons(proxyPort);
     proxy_address.sin_addr.s_addr = htonl(INADDR_ANY);
 
+
     Bind(proxySocket,(struct sockaddr *) & proxy_address, sizeof(proxy_address));
     Listen(proxySocket, SOMAXCONN);
     printf("Waiting for a connection on port: %d...\n", proxyPort);
     fflush(stdout);
    
-    ////accept client connection
-    //clientSocket = Accept(proxySocket, NULL, NULL);
+    while (1) {
+        //accept client connection
+        socklen_t proxy_addr_len = sizeof(proxy_address);
+        clientSocket = Accept(proxySocket,(struct sockaddr*) &proxy_address, &proxy_addr_len);
+        if (clientSocket < 0) continue;
+        
+        //handle client - proxy request
+        char client_headers[8192];
+        Metadata client_metadata;
+        if (handle_client(clientSocket, &client_metadata, &client_headers) < 0) {
+            close(clientSocket);
+            continue;
+        }
 
-    ////create server socket
-    //serverSocket = Socket(AF_INET, SOCK_STREAM, 0);
+        //connect to server
+        int serverSocket = handle_server(client_metadata.hostname, client_metadata.port);
+        if (serverSocket < 0) {
+            unix_error("Could not connect to server \n");
+            continue;
+        }
+
+        char outgoing_req[8192];
+        build_http_request(outgoing_req, client_metadata.hostname, client_metadata.path, client_headers);
+        printf("==============Forwarding request=================: \n%s\n============================\n", outgoing_req);
+
+        //send and receive 
+        send_to_server(serverSocket, outgoing_req);
+        relay_server_response(serverSocket, clientSocket);
+    }
+
 
     printf("%s", user_agent_hdr);
     close(proxySocket);
-    /*close(clientSocket);
-    close(clientSocket);*/
     return 0;
 }
--- proxy.h
+++ proxy.h
@@ -1,94 +1,153 @@
-#include <stdio.h>      
-#include <stdlib.h>     
-#include <string.h>  
-#include <unistd.h>
-#include <errno.h>
-#include <ctype.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include "csapp.h"
 
-void unix_error(char* msg) /* Unix-style error */
-{
-    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
-    exit(0);
-}
+/* You won't lose style points for including this long line in your code */
+static const char* user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
-/* $end unixerror */
+typedef struct {
+    char url[2048];
+    char hostname[1024];
+    char path[2048];
+    int port;
+} Metadata;
 
-void posix_error(int code, char* msg) /* Posix-style error */
-{
-    fprintf(stderr, "%s: %s\n", msg, strerror(code));
-    exit(0);
-}
+void parse_url(Metadata* client_metadata) {
+    char temp[2048];
+    strcpy(temp,client_metadata->url);
 
-void gai_error(int code, char* msg) /* Getaddrinfo-style error */
-{
-    fprintf(stderr, "%s: %s\n", msg, gai_strerror(code));
-    exit(0);
-}
+    client_metadata->port = 80;
+    strcpy(client_metadata->path, "/");
 
-void app_error(char* msg) /* Application error */
-{
-    fprintf(stderr, "%s\n", msg);
-    exit(0);
-}
-/* $end errorfuns */
+    const char* p = temp;
 
+    if (strncmp(p, "http://", 7) == 0) {
+        p += 7;
+    }
 
-//socket wrapper functions
-int Socket(int domain, int type, int protocol)
-{
-    int rc;
+    const char* slash = strchr(p, '/');
+    if (slash) {
+        strcpy(client_metadata->path, slash);
+    }
+    else {
+        strcpy(client_metadata->path, '/');
+    }
 
-    if ((rc = socket(domain, type, protocol)) < 0)
-        unix_error("Socket error");
-    return rc;
-}
+    char host_n_port[1024];  //extract host and port(maybe)
+    if (slash) {
+        strncpy(host_n_port, p, slash - p); //copy line from hostname till the firs '/' or path
+        host_n_port[slash - p] = '\0';
+    }
+    else {
+        strcpy(host_n_port, p); //copy all line starting from host name
+    }
 
-void Setsockopt(int s, int level, int optname, const void* optval, int optlen)
-{
-    int rc;
+    char* colon = strchr(host_n_port, ':');
+    if (colon) {
+        *colon = '\0';
+        strcpy(client_metadata->hostname, host_n_port);
 
-    if ((rc = setsockopt(s, level, optname, optval, optlen)) < 0)
-        unix_error("Setsockopt error");
-}
-
-void Bind(int sockfd, struct sockaddr* my_addr, int addrlen)
-{
-    int rc;
-
-    if ((rc = bind(sockfd, my_addr, addrlen)) < 0) {
-        close(sockfd);
-        unix_error("Bind error");
+        client_metadata->port = atoi(colon + 1);
+    }
+    else {
+        strcpy(client_metadata->hostname, host_n_port);
     }
 }
 
-void Listen(int s, int backlog)
-{
-    int rc;
+int handle_client(int clientSocket, Metadata* client_metadata, char* headers_out) {
+    rio_t rio;
+    Rio_readinitb(&rio, clientSocket);
 
-    if ((rc = listen(s, backlog)) < 0) {
-        close(s);
-        unix_error("Listen error");
+    char reqline[1024];
+    char method[16], url[2048], version[32];
+    char header[1024];
+
+    if (Rio_readlineb(&rio, reqline, 1024) <= 0) return -1;
+    sscanf(reqline, "%s %s %s", method, url, version);
+    
+    if (strcmp(method, "GET") != 0) {
+        fprintf(stderr, "Only GET supported \n");
+        return -1;
+    }
+
+    headers_out[0] = '\0';
+    while (Rio_readlineb(&rio, header, 1024) > 0) {
+        if (!strcmp(header, "\r\n")) break;
+        strcat(headers_out, header);
+    }
+
+    strcpy(client_metadata->url, url);
+    parse_url(client_metadata);
+
+    return 0;
+}
+
+int handle_server(const char* hostname, int port) {
+    int serverSocket;
+    struct addrinfo hints, *res;
+    char port_str[16];
+
+    sprintf(port_str, "%d", port);
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    Getaddrinfo(hostname, port_str, &hints, &res);
+
+    serverSocket = Socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (serverSocket < 0) {
+        Freeaddrinfo(res);
+        return -1;
+    }
+
+    Connect(serverSocket, res->ai_addr, res->ai_addrlen);
+
+    Freeaddrinfo(res);
+    return serverSocket;
+}
+
+void build_http_request(char* outbuf, const char* hostname, const char* path, const char* client_headers) {
+    
+    //put all in one string
+    sprintf(outbuf,
+        "GET %s HTTP/1.0\r\n"
+        "Host: %s\r\n"
+        "%s"
+        "Connection: close\r\n"
+        "Proxy-Connection: close\r\n",
+        path, hostname, user_agent_hdr);
+
+    strcat(outbuf, client_headers);
+    strcat(outbuf, "\r\n");
+}
+
+//send request to server(from proxy to server)
+//buffer safe send
+void send_to_server(int serverSocket,char* req_buffer) {
+    int sent = 0;
+    int left = strlen(req_buffer);
+    int n;
+
+    while (left > 0) {
+        n = send(serverSocket, req_buffer + sent, left, 0);
+        if (n <= 0) break;
+        sent += n;
+        left -= n;
     }
 }
 
-int Accept(int s, struct sockaddr* addr, socklen_t* addrlen)
-{
-    int rc;
+//receive and relay/send-back response to client (from server back to proxy back to client)
+//buffer safe
+void relay_server_response(int serverSocket,int clientSocket) {
+    char buf[8192];
+    int n;
 
-    if ((rc = accept(s, addr, addrlen)) < 0)
-        unix_error("Accept error");
-    return rc;
-}
-
-void Connect(int sockfd, struct sockaddr* serv_addr, int addrlen)
-{
-    int rc;
-
-    if ((rc = connect(sockfd, serv_addr, addrlen)) < 0)
-        unix_error("Connect error");
+    while ((n = recv(serverSocket, buf, sizeof(buf), 0)) > 0) {
+        int sent = 0;
+        while (sent < n) {
+            int s = send(clientSocket, buf + sent, n - sent, 0);
+            if (s <= 0) break;
+            sent += s;
+        }
+    }
 }
